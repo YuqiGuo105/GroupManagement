ee082372b4f5a12ddeda41c592ed1eb8b629cca2
Yuqi Guo <131561736+YuqiGuo105@users.noreply.github.com>
2025-03-02 20:05:33 -0700

    Initial Setup

---
+++ group-manage-service/src/main/java/org/example/groupmanageservice/GroupManageServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.groupmanageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupManageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupManageServiceApplication.class, args);
+    }
+
+}
---
+++ group-manage-service/src/test/java/org/example/groupmanageservice/GroupManageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.groupmanageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupManageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
